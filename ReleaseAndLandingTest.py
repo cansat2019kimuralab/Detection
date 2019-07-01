17e9d3067c14343e02b46ec7d26ae5d9ecc8904a
sorae19 <hp7316062@gmail.com>
2019-07-01 15:24:59 +0900

    Update ReleaseAndLandingTest.py

--- ReleaseAndLandingTest.py
+++ ReleaseAndLandingTest.py
@@ -78,6 +78,7 @@ if __name__ == "__main__":
 				#f.write(str(luxdata[0])+":"+str(luxdata[1]))
 				print("lux1: "+str(luxdata[0])+" "+"lux2: "+str(luxdata[1]))
 				f.write(str(luxdata[0])+"	"+str(luxdata[1])+ "\t")
+				f.write("\n")
 				
 				if luxdata[0]>500 or luxdata[1]>370:
 					lcount+=1
