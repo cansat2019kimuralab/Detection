eedd8416da24fce1519460072f621ff35a059ec2
unknown <hp7316062@gmail.com>
2019-07-15 12:47:31 +0900

    a

--- ReleaseAndLandingTest.py
+++ ReleaseAndLandingTest.py
@@ -44,11 +44,11 @@ def setup():
 	BME280.bme280_calib_param()
 	BMX055.bmx055_setup()
 	GPS.openGPS()
-	with open('log/releaseLog.txt', 'w') as f:
+	with open('log/releaseLog.txt', 'a') as f:
 		pass
-	with open('log/landingLog.txt', 'w') as f2:
+	with open('log/landingLog.txt', 'a') as f2:
 		pass
-	with open('log/runningLog.txt', 'w') as f3:
+	with open('log/runningLog.txt', 'a') as f3:
 		pass
 
 def close():
@@ -60,36 +60,38 @@ if __name__ == "__main__":
 	try:
 		setup()
 		time.sleep(t)
-		bme280Data=BME280.bme280_read()	
+		bme280Data=BME280.bme280_read()
 		lcount=0
 		acount=0
 		Pcount=0
 		GAcount=0
 		luxmax=300
-		deltAmax=0.1
-		deltPmax=0.1
+		deltAmax=0.3
+		deltPmax=0.05
 		deltHmax=5
-		
+
 		tx1 = time.time()
 		tx2 = tx1
 
 		#溶断まで
 		print("Releasing Judgement Program Start  {0}".format(time.time()))
-		#ループx
+
 		with open('log/releaseLog.txt', 'a') as f:
+			f.write("\n")
+			#ループx
 			while(1):
-				
+
 				#tx2を更新
 				tx2=time.time()
 				#放出判定(照度センサ)
 				luxdata=TSL2561.readLux()
 				#f.write(str(luxdata[0])+":"+str(luxdata[1]))
 				print("lux1: "+str(luxdata[0])+" "+"lux2: "+str(luxdata[1]))
-				f.write(str(luxdata[0])+"	"+str(luxdata[1])+ "\t")
-				f.write("\n")
+				f.write(str(luxdata[0])+"	"+str(luxdata[1])+"		")
+
 				print(lcount)
 				print(acount)
-				
+
 				if luxdata[0]>luxmax or luxdata[1]>luxmax:
 					lcount+=1
 				elif luxdata[0]<luxmax and luxdata[1]<luxmax:
@@ -99,59 +101,66 @@ if __name__ == "__main__":
 					print("luxreleasejudge")
 				else:
 					luxreleasejudge=False
-				#放出判定（気圧センサ）	
-					PRESS=bme280Data[1]       
+				#放出判定（気圧センサ）
+					PRESS=bme280Data[1]
 					deltA=PRESS
 					bme280Data=BME280.bme280_read()	#更新
 					PRESS=bme280Data[1]
-					deltA=deltA-PRESS
+					deltA=PRESS-deltA
 					#f.write("P0:P1"+str(P0)+":"+str(P1))
 					print(str(PRESS))
 					print(str(deltA))
-					time.sleep(3)
+					time.sleep(2)
 					#3秒前の値と比較
 					#高度差が式一以上でacout+=1
 					if deltA>deltAmax:
 						acount+=1
 					elif deltA<deltAmax:
 						acount=0
-					if acount>3:
+					if acount>2:
 						presreleasejudge=True
 						print("presjudge")
+
 					else:
 						presreleasejudge=False
-
+				f.write(str(deltA)+"   "+str(PRESS)+"\t")
+				f.write("\n")
 
 				if luxreleasejudge or presreleasejudge:
 					ty1=time.time()
 					ty2=ty1
 					print("RELEASE!")
-					bme280Data=BME280.bme280_read()	
+					f.write("release")
+					bme280Data=BME280.bme280_read()
 					gpsData = GPS.readGPS()
 					#ループyのタイムアウト判定
-					with open('log/runningLog.txt', 'w') as f2:
+					with open('log/landingLog.txt', 'a') as f2:
+						f.write("\n")
 						while(ty2-ty1<=y):
 							ty2=time.time()
 							#気圧判定
-							PRESS=bme280Data[1]       
+							PRESS=bme280Data[1]
 							deltP=PRESS
 							bme280Data=BME280.bme280_read()	#更新
 							PRESS=bme280Data[1]
 							deltP=deltP-PRESS
-							f.write("NEW: "+str(deltP)+"    old:"+str(PRESS)+ "\t")
-							print(PRESS)
+							f2.write(str(deltP))#+":"+str(PRESS)+ "\t")
+							f2.write("\n")
+							print(deltP)
+							print("Pcount:"+str(Pcount))
 							if abs(deltP)<deltPmax:
 								Pcount+=1
 							elif abs(deltP)>deltPmax:
 								Pcount=0
-							if Pcount>5:
+							if Pcount>4:
 								preslandjudge=True
 								print("preslandjudge")
 							else:
 								preslandjudge=False
+
 							#GPS高度判定
 							Gheight=gpsData[4]
-							deltH=Gheight	
+							deltH=Gheight
 							gpsData=GPS.readGPS()
 							Gheight=gpsData[4]
 							deltH=deltH-Gheight
@@ -162,7 +171,7 @@ if __name__ == "__main__":
 								GAcount+=1
 							elif abs(deltH)>deltHmax :
 								GAcount=0
-							if GAcount>5:
+							if GAcount>4:
 								GPSlandjudge=True
 								print("GPSlandjudge")
 							else:
@@ -174,13 +183,18 @@ if __name__ == "__main__":
 								print("satueinow")
 								#撮影
 							#両方に変化なければ着地、ループyを抜ける
-							else:
+							elif preslandjudge and  GPSlandjudge:
 								break
+							else:
+								print("Landingjudgement now")
 					#ループy中でbreakが起きなければ続行、起きたら全体も抜ける
-					else:
-						continue
-					print("THE ROVER HAS LANDED.  {0}".format(time.time()))
-					break
+						else:
+							print("THE ROVER HAS LANDED.  {0}".format(time.time()))
+							f2.write("land")
+							break
+						print("THE ROVER HAS LANDED.  {0}".format(time.time()))
+						f2.write("land")
+						break
 				#放出されず、かつループxでタイムアウト
 				elif tx2-tx1>=x:
 					tz1=time.time()
@@ -189,7 +203,7 @@ if __name__ == "__main__":
 					#ループzのタイムアウト判定
 					while(tz2-tz1<=z):
 						tz2=time.time()
-						PRESS=bme280Data[1]       
+						PRESS=bme280Data[1]
 						deltP=PRESS
 						bme280Data=BME280.bme280_read()	#更新
 						PRESS=bme280Data[1]
@@ -199,7 +213,7 @@ if __name__ == "__main__":
 						time.sleep(3)
 						if abs(deltP)<deltPmax:
 							Pcount+=1
-						elif abs(deltP)<deltPmax:
+						elif abs(deltP)>deltPmax:
 							Pcount=0
 						if Pcount>5:
 							preslandjudge=True
@@ -211,7 +225,7 @@ if __name__ == "__main__":
 							break
 					#ループz中でbreakが起きなければ続行、起きたら全体も抜ける
 					else:
-						continue
+						break
 					break
 #溶断へ
 		print("outcasing")
@@ -223,4 +237,4 @@ if __name__ == "__main__":
 		close()
 		IM920.Send("Error Occured")
 		IM920.Send("Program Stopped")
-		print(e.message)
+		print(e.message) 
