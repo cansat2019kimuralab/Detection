55ab77a2d09ccb399685b802112227a8e386f6b6
unknown <hp7316062@gmail.com>
2019-07-18 19:16:52 +0900

    Create Release.py
    add release

---
+++ ReleaseAndLandingDetection/Release.py
@@ -0,0 +1,53 @@
+import sys
+sys.path.append('/home/pi/git/kimuralab/SensorModuleTest/BME280')
+sys.path.append('/home/pi/git/kimuralab/SensorModuleTest/BMX055')
+sys.path.append('/home/pi/git/kimuralab/SensorModuleTest/Camera')
+sys.path.append('/home/pi/git/kimuralab/SensorModuleTest/GPS')
+sys.path.append('/home/pi/git/kimuralab/SensorModuleTest/IM920')
+sys.path.append('/home/pi/git/kimuralab/SensorModuleTest/Melting')
+sys.path.append('/home/pi/git/kimuralab/SensorModuleTest/Motor')
+sys.path.append('/home/pi/git/kimuralab/SensorModuleTest/TSL2561')
+import time
+import serial
+import pigpio
+import BME280
+import BMX055
+import IM920
+import GPS
+import Melting
+import Motor
+import TSL2561
+luxdata=[]
+lcount=0
+luxmax=200
+deltAmax=0.3
+
+def luxjudge():
+    luxdata=TSL2561.readLux()
+    if luxdata[0]>luxmax or luxdata[1]>luxmax:
+		lcount+=1
+	elif luxdata[0]<luxmax and luxdata[1]<luxmax:
+		lcount=0
+	if lcount>4:
+		luxjudge=1
+		print("luxreleasejudge")
+	else:
+		luxjudge=0
+    return luxjudge
+
+def pressjudge():
+	PRESS=bme280Data[1]
+	deltA=PRESS
+	bme280Data=BME280.bme280_read()	#更新
+	PRESS=bme280Data[1]
+	deltA=PRESS-deltA
+    if deltA>deltAmax:
+        acount+=1
+	elif deltA<deltAmax:
+		acount=0
+	if acount>2:
+		pressjudge=1
+		print("presjudge")
+    else:
+        pressjudge=0
+    return pressjudge
