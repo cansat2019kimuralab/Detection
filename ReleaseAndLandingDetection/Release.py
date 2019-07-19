bbf412489aee252829fbb43daf5e60629a700b9c
unknown <hp7316062@gmail.com>
2019-07-19 13:32:28 +0900

    Update Release.py
    float

--- ReleaseAndLandingDetection/Release.py
+++ ReleaseAndLandingDetection/Release.py
@@ -24,7 +24,7 @@ luxmax=200
 deltAmax=0.3
 presjudge=0
 luxjudge=0
-PRESS=[0,0]
+PRESS=[0.0,0.0]
 
 def luxjudge():
 	luxdata=TSL2561.readLux()
@@ -44,9 +44,9 @@ def luxjudge():
 def pressjudge():
 	global PRESS
 	global bme280Data
-	PRESS[0]=bme280Data[1]
+	PRESS[0]=float(bme280Data[1])
 	bme280Data=BME280.bme280_read()	#更新
-	PRESS[1]=bme280Data[1]
+	PRESS[1]=float(bme280Data[1])
 	deltA=PRESS[1]-PRESS[0]
 	if deltA>deltAmax:
 		acount+=1
