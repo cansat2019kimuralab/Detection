9928035fe8d20430716f624a22b4fd718f1bb7aa
arai <s-arai14@jcom.zaq.ne.jp>
2019-07-25 18:16:30 +0900

    20190725

--- GoalDetection/goal_detection.py
+++ GoalDetection/goal_detection.py
@@ -13,16 +13,13 @@ def GoalDetection(imgpath, H_min, H_max, S_thd):
 
 	#make mask
 	img_HSV = cv2.cvtColor(cv2.GaussianBlur(img,(15,15),0),cv2.COLOR_BGR2HSV_FULL)
-
 	h = img_HSV[:, :, 0]
 	s = img_HSV[:, :, 1]
-
 	mask = np.zeros(h.shape, dtype=np.uint8)
 	mask[((h < H_max) | (h > H_min)) & (s > S_thd)] = 255
 
 	#contour
 	mask, contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
 
 	#max area
 	max_area = 0
@@ -39,15 +36,14 @@ def GoalDetection(imgpath, H_min, H_max, S_thd):
 		return [-1,-1,imgname]
 		
 	cnt = contours[max_area_contour]
-
 	print('Max area is',max_area)
+
 	#no goal
 	if max_area <= 100:
 		print( "There is not the target" )
 		return [-1,-1,imgname]
 
 	#goal
-
 	if max_area >= 70000:
 		print( "GOAL" )
 		return [0,0,imgname]
