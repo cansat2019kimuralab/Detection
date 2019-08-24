fd066b9fde5860a98a841cffada83043f29dc17c
unknown <s-arai14@jcom.zaq.ne.jp>
2019-08-24 19:09:43 +0900

    try except

--- GoalDetection/goal_detection.py
+++ GoalDetection/goal_detection.py
@@ -6,6 +6,8 @@ import numpy as np
 import math
 import Capture
 
+i = 100
+
 def curvingSwitch(GAP, add):
 	if abs(GAP) > 144:
 		return add
@@ -39,52 +41,57 @@ def calR2G(nowArea, nowGAP, SampArea, SampL, SampX, SampGAP):
 	return [nowL, angR2G]
 
 def GoalDetection(imgpath, H_min, H_max, S_thd, G_thd):
-	imgname = Capture.Capture(imgpath)
-	img = cv2.imread(imgname)
-	hig, wid, col = img.shape
+	global i
+	try:
+		imgname = Capture.Capture(imgpath)
+		img = cv2.imread(imgname)
+		hig, wid, col = img.shape
 
-	#make mask
-	img_HSV = cv2.cvtColor(cv2.GaussianBlur(img,(15,15),0),cv2.COLOR_BGR2HSV_FULL)
-	h = img_HSV[:, :, 0]
-	s = img_HSV[:, :, 1]
-	mask = np.zeros(h.shape, dtype=np.uint8)
-	mask[((h < H_max) | (h > H_min)) & (s > S_thd)] = 255
+		#make mask
+		img_HSV = cv2.cvtColor(cv2.GaussianBlur(img,(15,15),0),cv2.COLOR_BGR2HSV_FULL)
+		h = img_HSV[:, :, 0]
+		s = img_HSV[:, :, 1]
+		mask = np.zeros(h.shape, dtype=np.uint8)
+		mask[((h < H_max) | (h > H_min)) & (s > S_thd)] = 255
 
-	#contour
-	mask, contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+		#contour
+		mask, contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-	#max area
-	max_area = 0
-	max_area_contour = -1
+		#max area
+		max_area = 0
+		max_area_contour = -1
 
-	for j in range(0,len(contours)):
-		area = cv2.contourArea(contours[j])
-		if max_area < area:
-			max_area = area
-			max_area_contour = j
+		for j in range(0,len(contours)):
+			area = cv2.contourArea(contours[j])
+			if max_area < area:
+				max_area = area
+				max_area_contour = j
 
-	#no goal
-	if max_area_contour == -1:
-		return [-1, 0, -1, imgname]
+		#no goal
+		if max_area_contour == -1:
+			return [-1, 0, -1, imgname]
 
-	elif max_area <= 30:
-		return [-1, 0, -1, imgname]
+		elif max_area <= 30:
+			return [-1, 0, -1, imgname]
 
-	#goal
-	elif max_area >= G_thd:
-		return [0, max_area ,0, imgname]
-	else:
-		#rectangle
-		cnt = contours[max_area_contour]
-		x,y,w,h = cv2.boundingRect(cnt)
-		GAP = x+w/2-160
-		return [1, max_area, GAP, imgname]
+		#goal
+		elif max_area >= G_thd:
+			return [0, max_area ,0, imgname]
+		else:
+			#rectangle
+			cnt = contours[max_area_contour]
+			x,y,w,h = cv2.boundingRect(cnt)
+			GAP = x+w/2-160
+			return [1, max_area, GAP, imgname]
+	except:
+		i = i + 1
+		return[i, -1, -1, imgname]
 
 if __name__ == "__main__":
 	try:
 		while 1:
 			goalflug, goalarea, goalGAP, photoname = GoalDetection("photo/photo",220 ,5, 180, 7000)
-			print("goalarea",goalarea, "goalGAP", goalGAP)
+			print("goalflug", goalflug, "goalarea",goalarea, "goalGAP", goalGAP, "name", photoname)
 	except KeyboardInterrupt:
 		print('stop')
 	except:
