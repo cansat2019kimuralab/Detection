341c46a004374d10ab480860d1cb194c7fce0776
arai <s-arai14@jcom.zaq.ne.jp>
2019-09-03 10:30:02 +0900

    20190903arai

--- ReleaseAndLandingDetection/releasephototest.py
+++ ReleaseAndLandingDetection/releasephototest.py
@@ -7,31 +7,26 @@ import math
 import Capture
 
 def DarkDetection(imgpath, V_thd, D_thd):
-	global i
-	try:
-		imgname = Capture.Capture(imgpath)
-		img = cv2.imread(imgname)
+	imgname = Capture.Capture(imgpath)
+	img = cv2.imread(imgname)
 
-		#make mask
-		img_HSV = cv2.cvtColor(cv2.GaussianBlur(img,(15,15),0),cv2.COLOR_BGR2HSV_FULL)
-		h = img_HSV[:, :, 0]
-		s = img_HSV[:, :, 1]
-		v = img_HSV[:, :, 2]
-		mask = np.zeros(h.shape, dtype=np.uint8)
-		mask[v < V_thd] = 255
+	#make mask
+	img_HSV = cv2.cvtColor(cv2.GaussianBlur(img,(15,15),0),cv2.COLOR_BGR2HSV_FULL)
+	h = img_HSV[:, :, 0]
+	s = img_HSV[:, :, 1]
+	v = img_HSV[:, :, 2]
+	mask = np.zeros(h.shape, dtype=np.uint8)
+	mask[v < V_thd] = 255
 
-		#black area
-		area = countNonZero(mask)
-		print('area is',area)
-        
-		if area > D_thd:
-			return [0, area, imgname]
-		else:
-			return [1, area, imgname]
+	#black area
+	area = cv2.countNonZero(mask)
+	print('area is',area)
+	
+	if area > D_thd:
+		return [0, area, imgname]
+	else:
+		return [1, area, imgname]
 
-	except:
-		print('error')
-		return [1, 0,  "Null"]
 
 if __name__ == "__main__":
 	try:
