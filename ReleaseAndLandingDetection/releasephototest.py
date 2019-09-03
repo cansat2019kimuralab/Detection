9c8488379a8dbc86ebbd646e5cafc02424ab25a9
arai <s-arai14@jcom.zaq.ne.jp>
2019-09-03 10:22:09 +0900

    20190903arai

--- ReleaseAndLandingDetection/releasephototest.py
+++ ReleaseAndLandingDetection/releasephototest.py
@@ -1,38 +1,44 @@
+
+import sys
+sys.path.append('/home/pi/git/kimuralab/SensorModuleTest/Camera')
 import cv2
 import numpy as np
 import math
+import Capture
 
-def DarkDetection(img, V_thd, D_thd):
-	cv2.imshow('Input Image',img)
+def DarkDetection(imgpath, V_thd, D_thd):
+	global i
+	try:
+		imgname = Capture.Capture(imgpath)
+		img = cv2.imread(imgname)
 
-	#make mask
-	img_HSV = cv2.cvtColor(cv2.GaussianBlur(img,(15,15),0),cv2.COLOR_BGR2HSV_FULL)
-	h = img_HSV[:, :, 0]
-	s = img_HSV[:, :, 1]
-	v = img_HSV[:, :, 2]
-	mask = np.zeros(h.shape, dtype=np.uint8)
-	mask[v < V_thd] = 255
+		#make mask
+		img_HSV = cv2.cvtColor(cv2.GaussianBlur(img,(15,15),0),cv2.COLOR_BGR2HSV_FULL)
+		h = img_HSV[:, :, 0]
+		s = img_HSV[:, :, 1]
+        v = img_HSV[:, :, 2]
+		mask = np.zeros(h.shape, dtype=np.uint8)
+		mask[v < V_thd] = 255
 
-	cv2.imshow('Red Zone', mask)
+		#black area
+        area = countNonZero(mask)
+        print('area is',area)
+        
+		if area > D_thd:
+            return [0, ,area, imgname]
+        else:
+            return [1, area, imgname]
 
-	#black area
-	area = cv2.countNonZero(mask)
-	print('area is',area)
-	
-	if area > D_thd:
-		cv2.waitKey()
-		cv2.destroyAllWindows()
-		return [0, area]
-	else:
-		cv2.waitKey()
-		cv2.destroyAllWindows()
-		return [1, area]
-
-	cv2.waitKey()
-	cv2.destroyAllWindows()
-	return [L,GAP]
+	except:
+		print('error')
+        return [1, 0,  "Null"]
 
 if __name__ == "__main__":
-	im = cv2.imread('photo3.jpg')
-	Darkflug, DarkArea= DarkDetection(im, 40, 30000)
-	print("Darkflug", Darkflug, "DarkArea",DarkArea)
+	try:
+		while 1:
+			Darkflug, DarkArea, photoname = DarkDetection("/home/pi/photo/photo", 40, 30000)
+			print("Darkflug", Darkflug, "DarkArea",DarkArea, "name", photoname)
+	except KeyboardInterrupt:
+		print('stop')
+	except:
+		print('error')
